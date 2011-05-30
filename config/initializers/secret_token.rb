2fe8bcd717b3155e27c8f385f7540cd4ee4027d3
GtK <GilliTheKid@GtK-MA.local>
2011-05-30 11:40:46 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lgr::Application.config.secret_token = '9970d64b0a1f2f4c44f851681c9383ebd6555e34a956208ee40f0862c42db9286040097bad2282a4592475ed9ef727c6641f091de20ca3fddcf24076a8067b07'
